31e94a65db4150626e04e18894fcb90b4f61632c
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2014-03-03 14:46:51 +0100

    Umstellung auf Maven Begonnen

---
+++ KoPeMe/src/main/java/de/kopeme/TestExecution.java
@@ -0,0 +1,160 @@
+package de.kopeme;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import de.kopeme.datacollection.CPUUsageCollector;
+import de.kopeme.datacollection.DataCollector;
+import de.kopeme.datacollection.TestResult;
+import de.kopeme.datacollection.TimeDataCollector;
+import de.kopeme.datastorage.YAMLDataStorer;
+
+/**
+ * Represents an execution of all runs of one test
+ * 
+ * @author dagere
+ * 
+ */
+public class TestExecution {
+	protected Class klasse;
+	protected Object instanz;
+	protected Method method;
+	protected int executionTimes, warmupExecutions;
+	protected Map<String, Double> maximalRelativeStandardDeviation;
+	protected Map<String, Long> assertationvalues;
+	protected String filename;
+
+	public TestExecution(Class klasse, Object instance, Method method) {
+		this.klasse = klasse;
+		this.instanz = instance;
+		this.method = method;
+
+		PerformanceTest annotation = method.getAnnotation(PerformanceTest.class);
+
+		if (annotation != null) {
+			executionTimes = annotation.executionTimes();
+			warmupExecutions = annotation.warmupExecutions();
+			maximalRelativeStandardDeviation = new HashMap<>();
+
+			for (MaximalRelativeStandardDeviation maxDev : annotation.deviations()) {
+				maximalRelativeStandardDeviation.put(maxDev.collectorname(), maxDev.maxvalue());
+			}
+
+			assertationvalues = new HashMap<>();
+			for (Assertion a : annotation.assertions()) {
+				assertationvalues.put(a.collectorname(), a.maxvalue());
+			}
+		}
+
+		filename = klasse.getName() + "." + method.getName();
+
+		// System.out.println("Run " + klasse);
+	}
+
+	public void runTest() {
+		try {
+
+			TestResult tr = new TestResult(filename, warmupExecutions);
+			if (method.getParameterTypes().length == 1) {
+				tr = executeComplexTest(tr);
+			} else {
+				tr = executeSimpleTest(tr);
+			}
+
+			if (!assertationvalues.isEmpty()) {
+				tr.checkValues(assertationvalues);
+			}
+
+			System.out.println("Beendet");
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException(
+				"Testmethoden von KoPeMe müssen genau einen Parameter vom Typ TestResult enthalten.");
+		} catch (IllegalAccessException e) {
+			// TODO Automatisch generierter Erfassungsblock
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Automatisch generierter Erfassungsblock
+			e.printStackTrace();
+		}
+	}
+
+	private TestResult executeComplexTest(TestResult tr) throws IllegalAccessException, InvocationTargetException {
+		Object[] params = { tr };
+
+		for (int i = 1; i <= warmupExecutions; i++) {
+			System.out.println("--- Starting warmup execution " + i + "/" + warmupExecutions + " ---");
+			method.invoke(instanz, params);
+			System.out.println("--- Stopping warmup execution " + i + "/" + warmupExecutions + " ---");
+		}
+
+		tr = new TestResult(filename, executionTimes);
+		params[0] = tr;
+		runMainExecution(tr, params, false);
+
+		tr.finalizeCollection();
+
+		tr.checkValues();
+		return tr;
+	}
+
+	private TestResult executeSimpleTest(TestResult tr) throws IllegalAccessException, InvocationTargetException {
+		Object[] params = {};
+
+		for (int i = 1; i <= warmupExecutions; i++) {
+			System.out.println("--- Starting warmup execution " + i + "/" + warmupExecutions + " ---");
+			method.invoke(instanz, params);
+			System.out.println("--- Stopping warmup execution " + i + "/" + warmupExecutions + " ---");
+		}
+
+		tr = new TestResult(filename, executionTimes);
+
+		runMainExecution(tr, params, true);
+
+		tr.finalizeCollection();
+
+		tr.checkValues();
+		return tr;
+	}
+
+	private void runMainExecution(TestResult tr, Object[] params, boolean simple) throws IllegalAccessException,
+		InvocationTargetException {
+
+		// if (maximalRelativeStandardDeviation == 0.0f){
+		for (int i = 1; i <= executionTimes; i++) {
+
+			System.out.println("--- Starting execution " + i + "/" + executionTimes + " ---");
+			if (simple)
+				tr.startCollection();
+			method.invoke(instanz, params);
+			if (simple)
+				tr.stopCollection();
+			System.out.println("--- Stopping execution " + i + "/" + executionTimes + " ---");
+			if (!maximalRelativeStandardDeviation.isEmpty() &&
+				tr.isRelativeStandardDeviationBelow(maximalRelativeStandardDeviation)){
+				break;
+			}
+		}
+		// }
+		// else{
+		// System.out.println("--- Starting execution until standard deviation is below "
+		// + maximalRelativeStandardDeviation + " ---");
+		// int i = 1;
+		// while
+		// (!tr.isRelativeStandardDeviationBelow(maximalRelativeStandardDeviation)
+		// && i < executionTimes){
+		// System.out.println("--- Starting execution " + i + " ---");
+		// if (simple) tr.startCollection();
+		// method.invoke(instanz, params);
+		// if (simple) tr.stopCollection();
+		// System.out.println("--- Stopping execution " + i + " ---");
+		// i++;
+		// }
+		// }
+
+	}
+}
---
+++ KoPeMe/src/main/java/de/kopeme/testrunner/PerformanceTestRunnerJUnit.java
@@ -0,0 +1,114 @@
+package de.kopeme.testrunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.experimental.theories.ParameterSignature;
+import org.junit.experimental.theories.ParametersSuppliedBy;
+import org.junit.experimental.theories.Theory;
+import org.junit.experimental.theories.Theories.TheoryAnchor;
+import org.junit.experimental.theories.internal.Assignments;
+import org.junit.internal.AssumptionViolatedException;
+import org.junit.internal.runners.model.ReflectiveCallable;
+import org.junit.internal.runners.statements.Fail;
+import org.junit.internal.runners.statements.InvokeMethod;
+import org.junit.runner.Description;
+import org.junit.runner.Runner;
+import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.junit.runners.model.FrameworkMethod;
+import org.junit.runners.model.InitializationError;
+import org.junit.runners.model.Statement;
+import org.junit.runners.model.TestClass;
+
+import de.kopeme.PerformanceTest;
+import de.kopeme.TestExecution;
+import de.kopeme.datacollection.TestResult;
+import de.kopeme.paralleltests.ParallelPerformanceTest;
+import de.kopeme.paralleltests.ParallelTestExecution;
+
+/**
+ * Runs a Performance Test with JUnit. The method which should be tested has to
+ * got the parameter TestResult. This does not work without another runner, e.g.
+ * the TheorieRunner. 
+ * An alternative implementation, e.g. via Rules, which would make it possible
+ * to include Theories, is not possible, because one needs to change the signature
+ * of test methods to get KoPeMe-Tests running.
+ * @author dagere
+ *
+ */
+public class PerformanceTestRunnerJUnit extends BlockJUnit4ClassRunner {
+
+	private String klasse;
+	
+	public PerformanceTestRunnerJUnit(Class<?> klasse)
+			throws InitializationError {
+		super(klasse);
+		this.klasse = klasse.getName();
+	}
+
+	@Override
+	protected void runChild(FrameworkMethod method, RunNotifier notifier) {
+		PerformanceTest a = method.getAnnotation(PerformanceTest.class);
+		if (a != null)
+			super.runChild(method, notifier);
+		else {
+			Description testBeschreibung = Description.createTestDescription(
+					this.getTestClass().getJavaClass(), method.getName());
+			notifier.fireTestIgnored(testBeschreibung);
+		}
+	}
+
+	@Override
+    protected void validateTestMethods(List<Throwable> errors) {
+        for (FrameworkMethod each : computeTestMethods()) {
+        	if ( each.getMethod().getParameterTypes().length != 1)
+        	{
+        		errors.add(new Exception("Method " + each.getName()
+                + " is supposed to have exactly one parameter, who's type is TestResult"));
+        	}
+        	if ( each.getMethod().getParameterTypes()[0] != TestResult.class)
+        	{
+        		errors.add(new Exception("Method " + each.getName()
+                        + " has wrong parameter Type: " + each.getMethod().getParameterTypes()[0]));
+        	}
+        }
+    }
+	
+	
+	@Override
+	protected Statement methodInvoker(FrameworkMethod method, Object test) {
+		return new PerformanceExecutionStatement(method, test);
+	}
+
+	public class PerformanceExecutionStatement extends Statement {
+
+		private final FrameworkMethod fTestMethod;
+		private Object fTarget;
+
+		public PerformanceExecutionStatement(FrameworkMethod testMethod,
+				Object target) {
+			fTestMethod = testMethod;
+			fTarget = target;
+		}
+
+		@Override
+		public void evaluate() throws Throwable {
+			TestExecution te;
+			if (fTestMethod.getAnnotation(ParallelPerformanceTest.class) != null)
+			{
+				te = new ParallelTestExecution(fTarget.getClass(), fTarget, fTestMethod.getMethod());
+			}
+			else
+			{
+				te = new TestExecution(fTarget.getClass(), fTarget, fTestMethod.getMethod());
+			}
+			te.runTest();
+//			String filename = klasse + "::" + fTestMethod.getName() + ".yml";
+//			int executions = 5;
+//			TestResult tr = new TestResult(filename, false, executions);
+//			fTestMethod.invokeExplosively(fTarget, tr);
+		}
+	}
+}
