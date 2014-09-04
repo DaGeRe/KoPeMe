7bb5947922e188011111c09fcb79da89fa4828df
David Georg Reichelt <reichelt@informatik.uni-leipzig.de>
2014-09-04 18:29:58 +0200

    Fehlerbehebung

--- kopeme-core/src/main/java/de/dagere/kopeme/datacollection/TemperatureCollector.java
+++ kopeme-core/src/main/java/de/dagere/kopeme/datacollection/TemperatureCollector.java
@@ -7,23 +7,23 @@ import java.io.InputStreamReader;
 
 public class TemperatureCollector {
 	public static int getTemperature(){
-		try {
-			Process p = Runtime.getRuntime().exec("acpi -t");
-			try {
-				p.waitFor();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			String line;
-			while ((line = br.readLine()) != null){
-				String s = line.substring(15, 19);
-				System.out.println("Line: " + s);
-			}
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+//		try {
+//			Process p = Runtime.getRuntime().exec("acpi -t");
+//			try {
+//				p.waitFor();
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
+//			BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+//			String line;
+//			while ((line = br.readLine()) != null){
+//				String s = line.substring(15, 19);
+//				System.out.println("Line: " + s);
+//			}
+//			
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 		
 		return 0;
 	}
