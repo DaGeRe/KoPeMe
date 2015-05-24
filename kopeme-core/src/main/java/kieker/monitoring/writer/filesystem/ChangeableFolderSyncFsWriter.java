c83907f859b68bfb5708cd860e7c1b3c2c25c888
dhaeb <dan.haeberlein@googlemail.com>
2015-05-24 13:39:02 +0200

    implement a kieker monitoring writer to be able to change the kieker output file at runtime

---
+++ kopeme-core/src/main/java/kieker/monitoring/writer/filesystem/ChangeableFolderSyncFsWriter.java
@@ -0,0 +1,100 @@
+package kieker.monitoring.writer.filesystem;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.Map.Entry;
+
+import kieker.common.configuration.Configuration;
+import kieker.common.record.IMonitoringRecord;
+import kieker.monitoring.writer.AbstractMonitoringWriter;
+
+public class ChangeableFolderSyncFsWriter extends AbstractMonitoringWriter {
+	
+	public static final String PREFIX = ChangeableFolderSyncFsWriter.class.getName() + ".";
+	public static final String CONFIG_PATH = PREFIX + "customStoragePath"; // NOCS (afterPREFIX)
+	public static final String CONFIG_MAXENTRIESINFILE = PREFIX + "maxEntriesInFile"; // NOCS (afterPREFIX)
+	public static final String CONFIG_MAXLOGSIZE = PREFIX + "maxLogSize"; // NOCS (afterPREFIX)
+	public static final String CONFIG_MAXLOGFILES = PREFIX + "maxLogFiles"; // NOCS (afterPREFIX)
+	public static final String CONFIG_FLUSH = PREFIX + "flush"; // NOCS (afterPREFIX)
+	public static final String CONFIG_BUFFER = PREFIX + "bufferSize"; // NOCS (afterPREFIX)
+	
+	private static ChangeableFolderSyncFsWriter INSTANCE = null;
+
+	public static synchronized ChangeableFolderSyncFsWriter getInstance(){
+		return INSTANCE;
+	}
+	
+	private SyncFsWriter defaultWriter;
+	private SyncFsWriter currentWriter;
+	private Configuration configuration;
+	
+	public ChangeableFolderSyncFsWriter(Configuration configuration) {
+		super(configuration);
+		this.configuration = configuration;
+		defaultWriter = new SyncFsWriter(toSyncFsWriterConfiguration(configuration));
+		if(INSTANCE != null){
+			throw new IllegalArgumentException("this is a bad singleton, so use it as one!");
+		}
+		INSTANCE = this;
+	}
+
+	Configuration toSyncFsWriterConfiguration(Configuration c){
+		Configuration returnable = new Configuration();
+		for (Iterator<Entry<Object, Object>> iterator = c.entrySet().iterator(); iterator.hasNext();) {
+			Entry<Object, Object> entry = iterator.next();
+			String keyAsString = entry.getKey().toString();
+			String replacedPropertyName = keyAsString.replace(getClass().getName(), SyncFsWriter.class.getName());
+			returnable.setProperty(replacedPropertyName, entry.getValue().toString());
+		}
+		return returnable;
+	}
+	
+	@Override
+	public synchronized boolean newMonitoringRecord(IMonitoringRecord record) {
+		if(currentWriter == null){
+			return defaultWriter.newMonitoringRecord(record);
+		} else {
+			return currentWriter.newMonitoringRecord(record);
+		}
+		
+	}
+
+	@Override
+	public synchronized void terminate() {
+		defaultWriter.terminate();
+		if(currentWriter != null){
+			currentWriter.terminate();
+		}
+			
+	}
+
+	@Override
+	protected void init() throws Exception {
+		defaultWriter.setController(monitoringController);
+	}
+	
+	public synchronized void setFolder(File writingFolder) throws Exception{
+		if(currentWriter != null){
+			currentWriter.terminate();
+		} 
+		writingFolder.mkdirs();
+		String absolutePath = writingFolder.getAbsolutePath();
+		Configuration resultingConfig = createTempConfigWithNewFolder(absolutePath);
+		currentWriter = new SyncFsWriter(resultingConfig);
+		currentWriter.setController(monitoringController);
+	}
+
+	private Configuration createTempConfigWithNewFolder(String absolutePath) {
+		Configuration tempConfig = toSyncFsWriterConfiguration(configuration);
+		tempConfig.setProperty(SyncFsWriter.CONFIG_PATH, absolutePath);
+		return tempConfig;
+	}
+	
+	public synchronized void closeCurrentWriter(){
+		if(currentWriter != null){
+			currentWriter.terminate();
+		}
+		currentWriter = null;
+	}
+
+}
---
+++ kopeme-core/src/test/java/kieker/monitoring/writer/filesystem/TestChangeableFoolderSyncFsWriter.java
@@ -0,0 +1,127 @@
+package kieker.monitoring.writer.filesystem;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.concurrent.ExecutionException;
+
+import kieker.common.configuration.Configuration;
+import kieker.common.record.controlflow.OperationExecutionRecord;
+import kieker.monitoring.core.configuration.ConfigurationFactory;
+import kieker.monitoring.core.controller.IMonitoringController;
+import kieker.monitoring.core.controller.MonitoringController;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import de.dagere.kopeme.TestUtils;
+
+public class TestChangeableFoolderSyncFsWriter {
+
+	private static IMonitoringController MONITORING_CONTROLLER;
+
+	private static final File DEFAULT_FOLDER = new File("target/test-classes/kieker_testresults");
+	private static final File NEW_FOLDER_AT_RUNTIME = new File("target/test-classes/kieker_testresults_changed_folder");
+	
+	@BeforeClass
+	public static void setupClass(){
+		TestUtils.deleteRecursively(DEFAULT_FOLDER);
+		TestUtils.deleteRecursively(NEW_FOLDER_AT_RUNTIME);
+		DEFAULT_FOLDER.mkdirs();
+		NEW_FOLDER_AT_RUNTIME.mkdirs();
+		Configuration config = ConfigurationFactory.createSingletonConfiguration();
+		String absolutePath = DEFAULT_FOLDER.getAbsolutePath();
+		config.setProperty("kieker.monitoring.writer", ChangeableFolderSyncFsWriter.class.getName());
+		config.setProperty(ChangeableFolderSyncFsWriter.CONFIG_PATH, absolutePath);
+		config.setProperty(ChangeableFolderSyncFsWriter.CONFIG_MAXENTRIESINFILE, "100");
+		config.setProperty(ChangeableFolderSyncFsWriter.CONFIG_MAXLOGFILES, "-1");
+		config.setProperty(ChangeableFolderSyncFsWriter.CONFIG_MAXLOGSIZE, "1");
+		config.setProperty(ChangeableFolderSyncFsWriter.CONFIG_FLUSH, "true");
+		config.setProperty(ChangeableFolderSyncFsWriter.CONFIG_BUFFER, "8192");
+		MONITORING_CONTROLLER = MonitoringController.createInstance(config);
+	}
+	
+	private static void createAndWriteOperationExecutionRecord(final long tin, final long tout, String methodSignature) {
+		final OperationExecutionRecord e = new OperationExecutionRecord(
+				methodSignature,
+				OperationExecutionRecord.NO_SESSION_ID,
+				OperationExecutionRecord.NO_TRACE_ID,
+				tin, tout, "myHost",
+				OperationExecutionRecord.NO_EOI_ESS,
+				OperationExecutionRecord.NO_EOI_ESS);
+				MONITORING_CONTROLLER.newMonitoringRecord(e);
+	}
+	
+	public static class Sample {
+		public void a() throws InterruptedException, ExecutionException{
+			final long tin = MONITORING_CONTROLLER.getTimeSource().getTime();
+			b();
+			final long tout = MONITORING_CONTROLLER.getTimeSource().getTime();
+			createAndWriteOperationExecutionRecord(tin, tout, "public void " + Sample.class.getName() + ".b()");
+			Thread.sleep(5L);
+		}
+
+		private void b() throws InterruptedException {
+			final long tin = MONITORING_CONTROLLER.getTimeSource().getTime();
+			c();
+			final long tout = MONITORING_CONTROLLER.getTimeSource().getTime();
+			createAndWriteOperationExecutionRecord(tin, tout, "public void " + Sample.class.getName() + ".c)");
+		}
+
+		protected void c() throws InterruptedException {
+			Thread.sleep(5L);
+		}
+	}
+
+	@Test
+	public void testConfigConvertion() throws Exception {
+		Configuration c = new Configuration();
+		int fixture = 100;
+		c.setProperty(ChangeableFolderSyncFsWriter.CONFIG_MAXENTRIESINFILE, Integer.toString(fixture));
+		ChangeableFolderSyncFsWriter testable = ChangeableFolderSyncFsWriter.getInstance();
+		Configuration result = testable.toSyncFsWriterConfiguration(c);
+		int intResult = result.getIntProperty(SyncFsWriter.CONFIG_MAXENTRIESINFILE);
+		assertEquals(fixture, intResult);
+	}
+	
+	@Test
+	public void testChangesFolderCorrectly() throws Exception {
+		ChangeableFolderSyncFsWriter testable = ChangeableFolderSyncFsWriter.getInstance();
+		int rounds = 10, lines = rounds / 2 * 3;
+		for(int i = 0; i < rounds; i++){
+			if((i+1)% 6 == 0){
+				testable.setFolder(NEW_FOLDER_AT_RUNTIME);
+			}
+			Sample fixture = new Sample();
+			final long tin = MONITORING_CONTROLLER.getTimeSource().getTime();
+			fixture.a();
+			final long tout = MONITORING_CONTROLLER.getTimeSource().getTime();
+			createAndWriteOperationExecutionRecord(tin, tout, "public void " + Sample.class.getName() + ".a()");
+		}
+		assertKiekerFileConstainsLines(DEFAULT_FOLDER, lines + 1); // TODO due to the meta data entry, which is not written when changing the folder
+		assertKiekerFileConstainsLines(NEW_FOLDER_AT_RUNTIME, lines);
+	}
+
+	private void assertKiekerFileConstainsLines(File kiekerFolder, int lines) throws IOException {
+		File[] listFiles = kiekerFolder.listFiles();
+		assertEquals(1, listFiles.length); // only the kieker root dir
+		File kiekerRootDir = listFiles[0];
+		assertTrue("Kieker root dir should be a directory!", kiekerRootDir.isDirectory());
+		File[] kiekerFiles = kiekerRootDir.listFiles();
+		assertEquals("There should be 2 kieker files!", 2, kiekerFiles.length);
+		File[] measureFile = kiekerRootDir.listFiles(new FileFilter() {
+			
+			@Override
+			public boolean accept(File pathname) {
+				return !pathname.getName().equals("kieker.map");
+			}
+		});
+		assertEquals(1, measureFile.length);
+		File currentMeasureFile = measureFile[0];
+		assertEquals(lines, Files.readAllLines(currentMeasureFile.toPath()).size());
+	}
+	
+}
