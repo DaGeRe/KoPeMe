7a0b5af906ab89c48c3eca35490c8a4fffc68f10
dgr <reichelt@informatik.uni-leipzig.de>
2015-05-28 10:08:19 +0200

    Kleinere Anpassungen

--- kopeme-junit/src/main/java/de/dagere/kopeme/junit/rule/annotations/BeforeNoMeasurement.java
+++ kopeme-junit/src/main/java/de/dagere/kopeme/junit/rule/annotations/BeforeNoMeasurement.java
@@ -6,6 +6,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation for a method that should be run before a test without measuring the performance.
+ * 
+ * @author reichelt
+ *
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ METHOD })
 public @interface BeforeNoMeasurement {
