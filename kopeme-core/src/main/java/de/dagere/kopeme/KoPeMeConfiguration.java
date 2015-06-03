ca9fdd15c4b9b07dacdb7ddeb9e0de275b25d093
dhaeb <dan.haeberlein@googlemail.com>
2015-06-03 09:06:08 +0200

    #25 add documentation

--- kopeme-core/src/main/java/de/dagere/kopeme/KoPeMeConfiguration.java
+++ kopeme-core/src/main/java/de/dagere/kopeme/KoPeMeConfiguration.java
@@ -1,9 +1,21 @@
 package de.dagere.kopeme;
 
+/**
+ * Configuration Singleton class which can return the projectname of the current project, 
+ * set by VM parameter or project model files (e.g. pom.xml) 
+ * 
+ * @author dhaeb
+ *
+ */
 public class KoPeMeConfiguration {
 
 	private static KoPeMeConfiguration INSTANCE = null;
 	
+	/**
+	 * The public interface to retrieve the configuration instance. 
+	 * 
+	 * @return
+	 */
 	public static synchronized KoPeMeConfiguration getInstance(){
 		if(INSTANCE == null){
 			INSTANCE = new KoPeMeConfiguration();
@@ -11,11 +23,23 @@ public class KoPeMeConfiguration {
 		return INSTANCE;
 	}
 	
+	/**
+	 * Default projectname if no VM parameter is given or no pom.xml can be extracted.
+	 */
 	public final static String DEFAULT_PROJECTNAME = "default";
+	
+	
+	/**
+	 * VM property name for the projectname.
+	 * If given, this property will be used, regardless if there exists a pom.xml
+	 */
 	static final String KOPEME_PROJECTNAME_PROPNAME = "kopeme.projectname";
 	
 	private String projectName;
 	
+	/**
+	 * package scope for testinig purposes
+	 */
 	KoPeMeConfiguration() {
 		projectName = System.getProperty(KOPEME_PROJECTNAME_PROPNAME, DEFAULT_PROJECTNAME) ;
 	}
