0a212af0818f1de43df9596eff9e8c537380f0ca
dgr <reichelt@informatik.uni-leipzig.de>
2016-05-23 13:11:44 +0200

    Fehlerbehebung

--- kopeme-junit/src/main/java/de/dagere/kopeme/junit/rule/throughput/ComplexThroughputStatement.java
+++ kopeme-junit/src/main/java/de/dagere/kopeme/junit/rule/throughput/ComplexThroughputStatement.java
@@ -2,9 +2,7 @@ package de.dagere.kopeme.junit.rule.throughput;
 
 import static de.dagere.kopeme.PerformanceTestUtils.saveData;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Map;
 
 import junit.framework.AssertionFailedError;
 
@@ -36,23 +34,22 @@ public class ComplexThroughputStatement extends KoPeMeBasicStatement {
 
 	@Override
 	public void evaluate() throws Throwable {
-		String methodString = method.getClass().getName() + "." + method.getName();
-		runWarmup(methodString);
+		runMainExecution(new TestResult(method.getName(), annotation.warmupExecutions(), DataCollectorList.STANDARD), "warmup execution ", annotation.warmupExecutions());
 
 		while (currentsize <= maxsize) {
-			TestResult tr = new TestResult(method.getName(), annotation.executionTimes(), DataCollectorList.STANDARD);
+			final TestResult tr = new TestResult(method.getName(), annotation.executionTimes(), DataCollectorList.STANDARD);
 
 			if (!checkCollectorValidity(tr)) {
 				log.warn("Not all Collectors are valid!");
 			}
 
 			try {
-				runMainExecution(tr);
-			} catch (AssertionFailedError t) {
+				runMainExecution(tr, "execution ", annotation.executionTimes());
+			} catch (final AssertionFailedError t) {
 				tr.finalizeCollection();
 				saveData(SaveableTestData.createAssertFailedTestData(method.getName(), filename, tr, 0, true));
 				throw t;
-			} catch (Throwable t) {
+			} catch (final Throwable t) {
 				tr.finalizeCollection();
 				saveData(SaveableTestData.createErrorTestData(method.getName(), filename, tr, 0, true));
 				throw t;
@@ -69,29 +66,29 @@ public class ComplexThroughputStatement extends KoPeMeBasicStatement {
 		}
 	}
 
-	@Override
-	protected void runMainExecution(final TestResult tr) throws IllegalAccessException, InvocationTargetException {
-		int executions;
-		int executionTimes = annotation.executionTimes();
-		for (executions = 1; executions <= executionTimes; executions++) {
-
-			log.debug("--- Starting execution " + executions + "/" + executionTimes + " ---");
-			runnables.getBeforeRunnable().run();
-			tr.startCollection();
-			runnables.getTestRunnable().run();
-			tr.stopCollection();
-			runnables.getAfterRunnable().run();
-
-			log.debug("--- Stopping execution " + executions + "/" + executionTimes + " ---");
-			for (Map.Entry<String, Double> entry : maximalRelativeStandardDeviation.entrySet()) {
-				log.trace("Entry: {} {}", entry.getKey(), entry.getValue());
-			}
-			if (executions >= annotation.minEarlyStopExecutions() && !maximalRelativeStandardDeviation.isEmpty()
-					&& tr.isRelativeStandardDeviationBelow(maximalRelativeStandardDeviation)) {
-				break;
-			}
-		}
-		log.debug("Executions: " + (executions - 1));
-		tr.setRealExecutions(executions - 1);
-	}
+//	@Override
+//	protected void runMainExecution(final TestResult tr) throws IllegalAccessException, InvocationTargetException {
+//		int executions;
+//		int executionTimes = annotation.executionTimes();
+//		for (executions = 1; executions <= executionTimes; executions++) {
+//
+//			log.debug("--- Starting execution " + executions + "/" + executionTimes + " ---");
+//			runnables.getBeforeRunnable().run();
+//			tr.startCollection();
+//			runnables.getTestRunnable().run();
+//			tr.stopCollection();
+//			runnables.getAfterRunnable().run();
+//
+//			log.debug("--- Stopping execution " + executions + "/" + executionTimes + " ---");
+//			for (Map.Entry<String, Double> entry : maximalRelativeStandardDeviation.entrySet()) {
+//				log.trace("Entry: {} {}", entry.getKey(), entry.getValue());
+//			}
+//			if (executions >= annotation.minEarlyStopExecutions() && !maximalRelativeStandardDeviation.isEmpty()
+//					&& tr.isRelativeStandardDeviationBelow(maximalRelativeStandardDeviation)) {
+//				break;
+//			}
+//		}
+//		log.debug("Executions: " + (executions - 1));
+//		tr.setRealExecutions(executions - 1);
+//	}
 }
--- kopeme-junit/src/main/java/de/dagere/kopeme/junit/rule/throughput/ThroughputStatement.java
+++ kopeme-junit/src/main/java/de/dagere/kopeme/junit/rule/throughput/ThroughputStatement.java
@@ -2,9 +2,7 @@ package de.dagere.kopeme.junit.rule.throughput;
 
 import static de.dagere.kopeme.PerformanceTestUtils.saveData;
 
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Map;
 
 import junit.framework.AssertionFailedError;
 
@@ -31,24 +29,24 @@ public class ThroughputStatement extends KoPeMeBasicStatement {
 
 	@Override
 	public void evaluate() throws Throwable {
-		String methodString = method.getClass().getName() + "." + method.getName();
-		runWarmup(methodString);
+		final String methodString = method.getClass().getName() + "." + method.getName();
+		runMainExecution(new TestResult(method.getName(), annotation.warmupExecutions(), DataCollectorList.STANDARD), "warmup execution ", annotation.warmupExecutions());
 
 		int executionTimes = annotation.executionTimes();
 		while (executionTimes <= maxsize) {
-			TestResult tr = new TestResult(method.getName(), executionTimes, DataCollectorList.STANDARD);
+			final TestResult tr = new TestResult(method.getName(), executionTimes, DataCollectorList.STANDARD);
 
 			if (!checkCollectorValidity(tr)) {
 				log.warn("Not all Collectors are valid!");
 			}
 
 			try {
-				runMainExecution(tr);
-			} catch (AssertionFailedError t) {
+				runMainExecution(tr, "execution ", annotation.executionTimes());
+			} catch (final AssertionFailedError t) {
 				tr.finalizeCollection();
 				saveData(SaveableTestData.createAssertFailedTestData(method.getName(), filename, tr, 0, true));
 				throw t;
-			} catch (Throwable t) {
+			} catch (final Throwable t) {
 				tr.finalizeCollection();
 				saveData(SaveableTestData.createErrorTestData(method.getName(), filename, tr, 0, true));
 				throw t;
@@ -65,29 +63,28 @@ public class ThroughputStatement extends KoPeMeBasicStatement {
 		// PerformanceTestUtils.saveData(method.getName(), tr, false, false, filename, true);
 	}
 
-	@Override
-	protected void runMainExecution(final TestResult tr) throws IllegalAccessException, InvocationTargetException {
-		int executions;
-		int executionTimes = annotation.executionTimes();
-		for (executions = 1; executions <= executionTimes; executions++) {
-
-			log.debug("--- Starting execution " + executions + "/" + executionTimes + " ---");
-			runnables.getBeforeRunnable().run();
-			tr.startOrRestartCollection();
-			runnables.getTestRunnable().run();
-			tr.stopCollection();
-			runnables.getAfterRunnable().run();
-
-			log.debug("--- Stopping execution " + executions + "/" + executionTimes + " ---");
-			for (Map.Entry<String, Double> entry : maximalRelativeStandardDeviation.entrySet()) {
-				log.trace("Entry: {} {}", entry.getKey(), entry.getValue());
-			}
-			if (executions >= annotation.minEarlyStopExecutions() && !maximalRelativeStandardDeviation.isEmpty()
-					&& tr.isRelativeStandardDeviationBelow(maximalRelativeStandardDeviation)) {
-				break;
-			}
-		}
-		log.debug("Executions: " + (executions - 1));
-		tr.setRealExecutions(executions - 1);
-	}
+//	protected void runMainExecution(final TestResult tr) throws IllegalAccessException, InvocationTargetException {
+//		int executions;
+//		final int executionTimes = annotation.executionTimes();
+//		for (executions = 1; executions <= executionTimes; executions++) {
+//
+//			log.debug("--- Starting execution " + executions + "/" + executionTimes + " ---");
+//			runnables.getBeforeRunnable().run();
+//			tr.startOrRestartCollection();
+//			runnables.getTestRunnable().run();
+//			tr.stopCollection();
+//			runnables.getAfterRunnable().run();
+//
+//			log.debug("--- Stopping execution " + executions + "/" + executionTimes + " ---");
+//			for (final Map.Entry<String, Double> entry : maximalRelativeStandardDeviation.entrySet()) {
+//				log.trace("Entry: {} {}", entry.getKey(), entry.getValue());
+//			}
+//			if (executions >= annotation.minEarlyStopExecutions() && !maximalRelativeStandardDeviation.isEmpty()
+//					&& tr.isRelativeStandardDeviationBelow(maximalRelativeStandardDeviation)) {
+//				break;
+//			}
+//		}
+//		log.debug("Executions: " + (executions - 1));
+//		tr.setRealExecutions(executions - 1);
+//	}
 }
