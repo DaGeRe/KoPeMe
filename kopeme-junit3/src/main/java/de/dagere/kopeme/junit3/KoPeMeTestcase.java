afd6314a77908f7223c2c8761c2b48f2873bc44d
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2018-03-28 12:22:52 +0200

    Extended Error Logging

--- kopeme-junit3/src/main/java/de/dagere/kopeme/junit3/KoPeMeTestcase.java
+++ kopeme-junit3/src/main/java/de/dagere/kopeme/junit3/KoPeMeTestcase.java
@@ -26,242 +26,239 @@ import junit.framework.TestCase;
  */
 public abstract class KoPeMeTestcase extends TestCase {
 
-	private static final int INTERRUPT_TRIES = 10;
+   private static final int INTERRUPT_TRIES = 10;
 
-	private static final Logger LOG = LogManager.getLogger(KoPeMeTestcase.class);
+   private static final Logger LOG = LogManager.getLogger(KoPeMeTestcase.class);
 
-	private final PerformanceTest annoTestcase = AnnotationDefaults.of(PerformanceTest.class);
-	private final PerformanceTestingClass annoTestClass = AnnotationDefaults.of(PerformanceTestingClass.class);
+   private final PerformanceTest annoTestcase = AnnotationDefaults.of(PerformanceTest.class);
+   private final PerformanceTestingClass annoTestClass = AnnotationDefaults.of(PerformanceTestingClass.class);
 
-	/**
-	 * Initializes the testcase.
-	 */
-	public KoPeMeTestcase() {
-	}
+   /**
+    * Initializes the testcase.
+    */
+   public KoPeMeTestcase() {
+   }
 
-	/**
-	 * Initializes the testcase with its name.
-	 * 
-	 * @param name Name of the testcase
-	 */
-	public KoPeMeTestcase(final String name) {
-		super(name);
-	}
+   /**
+    * Initializes the testcase with its name.
+    * 
+    * @param name Name of the testcase
+    */
+   public KoPeMeTestcase(final String name) {
+      super(name);
+   }
 
-	/**
-	 * Returns the count of warmup executions, default is 1.
-	 * 
-	 * @return Warmup executions
-	 */
-	protected int getWarmupExecutions() {
-		return annoTestcase.warmupExecutions();
-	}
+   /**
+    * Returns the count of warmup executions, default is 1.
+    * 
+    * @return Warmup executions
+    */
+   protected int getWarmupExecutions() {
+      return annoTestcase.warmupExecutions();
+   }
 
-	/**
-	 * Returns the count of repetitions, default is 1.
-	 * 
-	 * @return Repetitions executions
-	 */
-	protected int getRepetitions() {
-		return annoTestcase.repetitions();
-	}
+   /**
+    * Returns the count of repetitions, default is 1.
+    * 
+    * @return Repetitions executions
+    */
+   protected int getRepetitions() {
+      return annoTestcase.repetitions();
+   }
 
-	/**
-	 * Returns the count of real executions.
-	 * 
-	 * @return real executions
-	 */
-	protected int getExecutionTimes() {
-		return annoTestcase.executionTimes();
-	}
+   /**
+    * Returns the count of real executions.
+    * 
+    * @return real executions
+    */
+   protected int getExecutionTimes() {
+      return annoTestcase.executionTimes();
+   }
 
-	/**
-	 * Returns weather full data should be logged.
-	 * 
-	 * @return Weather full data should be logged
-	 */
-	protected boolean logFullData() {
-		return annoTestcase.logFullData();
-	}
+   /**
+    * Returns weather full data should be logged.
+    * 
+    * @return Weather full data should be logged
+    */
+   protected boolean logFullData() {
+      return annoTestcase.logFullData();
+   }
 
-	/**
-	 * Returns the time all testcase executions may take *in sum* in ms. -1 means unbounded; Standard is set to 120 s.
-	 * 
-	 * @return Maximal time of all test executions
-	 */
-	protected long getMaximalTime() {
-		return annoTestClass.overallTimeout();
-	}
+   /**
+    * Returns the time all testcase executions may take *in sum* in ms. -1 means unbounded; Standard is set to 120 s.
+    * 
+    * @return Maximal time of all test executions
+    */
+   protected long getMaximalTime() {
+      return annoTestClass.overallTimeout();
+   }
 
-	/**
-	 * Gets the list of datacollectors for the current execution.
-	 * 
-	 * @return List of Datacollectors
-	 */
-	protected DataCollectorList getDataCollectors() {
-		return DataCollectorList.STANDARD;
-	}
+   /**
+    * Gets the list of datacollectors for the current execution.
+    * 
+    * @return List of Datacollectors
+    */
+   protected DataCollectorList getDataCollectors() {
+      return DataCollectorList.STANDARD;
+   }
 
-	/**
-	 * Should kieker monitoring be used.
-	 * 
-	 * @return
-	 */
-	protected boolean useKieker() {
-		return annoTestcase.useKieker();
-	}
+   /**
+    * Should kieker monitoring be used.
+    * 
+    * @return
+    */
+   protected boolean useKieker() {
+      return annoTestcase.useKieker();
+   }
 
-	@Override
-	public void runBare() throws InterruptedException {
-		LOG.trace("Initialize JUnit-3-KoPeMe-Testcase");
+   @Override
+   public void runBare() throws InterruptedException {
+      LOG.trace("Initialize JUnit-3-KoPeMe-Testcase");
 
-		final int warmupExecutions = getWarmupExecutions(), executionTimes = getExecutionTimes();
-		final boolean fullData = logFullData();
-		final long timeoutTime = getMaximalTime();
+      final int warmupExecutions = getWarmupExecutions(), executionTimes = getExecutionTimes();
+      final boolean fullData = logFullData();
+      final long timeoutTime = getMaximalTime();
 
-		final String testClassName = this.getClass().getName();
-		final TestResult tr = new TestResult(testClassName, executionTimes, DataCollectorList.STANDARD);
-		tr.setCollectors(getDataCollectors());
+      final String testClassName = this.getClass().getName();
+      final TestResult tr = new TestResult(testClassName, executionTimes, DataCollectorList.STANDARD);
+      tr.setCollectors(getDataCollectors());
 
-		try {
-			KoPeMeKiekerSupport.INSTANCE.useKieker(useKieker(), testClassName, getName());
-		} catch (final Exception e) {
-			System.err.println("Kieker has failed!");
-			e.printStackTrace();
-		}
+      try {
+         KoPeMeKiekerSupport.INSTANCE.useKieker(useKieker(), testClassName, getName());
+      } catch (final Exception e) {
+         System.err.println("Kieker has failed!");
+         e.printStackTrace();
+      }
 
-		final Thread thread = new Thread(new Runnable() {
+      final Thread thread = new Thread(new Runnable() {
 
-			@Override
-			public void run() {
-				try {
-					runTestCase(tr, warmupExecutions, executionTimes, fullData);
-				} catch (final InvocationTargetException e) {
-					e.printStackTrace();
-				} catch (final IllegalAccessException e) {
-					e.printStackTrace();
-				} catch (final AssertionFailedError e) {
-					e.printStackTrace();
-				} catch (final Throwable e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				tr.finalizeCollection();
-				LOG.debug("Test-call finished");
-			}
-		});
+         @Override
+         public void run() {
+            try {
+               runTestCase(tr, warmupExecutions, executionTimes, fullData);
+            } catch (final AssertionFailedError | IllegalAccessException | InvocationTargetException e) {
+               e.printStackTrace();
+            } catch (final Throwable e) {
+               e.printStackTrace();
+            }
+            tr.finalizeCollection();
+            LOG.debug("Test-call finished");
+         }
+      });
 
-		thread.setUncaughtExceptionHandler(new UncaughtExceptionHandler() {
+      thread.setUncaughtExceptionHandler(new UncaughtExceptionHandler() {
 
-			@Override
-			public void uncaughtException(final Thread t, final Throwable e) {
-				if (e instanceof OutOfMemoryError) {
-					while (t.isAlive()) {
-						t.interrupt();
-					}
-				}
-				e.printStackTrace();
-				fail();
-			}
-		});
+         @Override
+         public void uncaughtException(final Thread t, final Throwable e) {
+            if (e instanceof OutOfMemoryError) {
+               while (t.isAlive()) {
+                  t.interrupt();
+               }
+            }
+            e.printStackTrace();
+            fail();
+         }
+      });
 
-		LOG.debug("Waiting for test-completion for {}", timeoutTime);
-		waitForTestEnd(timeoutTime, thread);
-		// No matter how the test gets finished, saving should be done here
-		LOG.trace("End-Testcase-Saving begins");
-		PerformanceTestUtils.saveData(SaveableTestData.createFineTestData(getName(), getClass().getName(), tr, warmupExecutions, fullData));
+      LOG.debug("Waiting for test-completion for {}", timeoutTime);
+      waitForTestEnd(timeoutTime, thread);
+      // No matter how the test gets finished, saving should be done here
+      LOG.trace("End-Testcase-Saving begins");
+      PerformanceTestUtils.saveData(SaveableTestData.createFineTestData(getName(), getClass().getName(), tr, warmupExecutions, fullData));
 
-		LOG.debug("KoPeMe-Test {} finished", getName());
-	}
+      LOG.debug("KoPeMe-Test {} finished", getName());
+   }
 
-	private void waitForTestEnd(final long timeoutTime, final Thread thread) throws InterruptedException {
-		thread.start();
+   private void waitForTestEnd(final long timeoutTime, final Thread thread) throws InterruptedException {
+      thread.start();
 
-		thread.join(timeoutTime);
-		LOG.trace("Test should be finished...");
-		if (thread.isAlive()) {
-			int count = 0;
-			while (thread.isAlive() && count < INTERRUPT_TRIES) {
-				LOG.debug("Thread not finished, is kill now..");
-				thread.interrupt();
-				Thread.sleep(10);
-				count++;
-			}
-			if (count == INTERRUPT_TRIES) {
-				LOG.debug("Thread does not respond, so it is killed hard now.");
-				count = 0;
-				while (thread.isAlive() && count < 5) {
-					thread.stop();
-					Thread.sleep(10);
-					count++;
-				}
-			}
-		}
-	}
+      thread.join(timeoutTime);
+      LOG.trace("Test should be finished...");
+      if (thread.isAlive()) {
+         int count = 0;
+         while (thread.isAlive() && count < INTERRUPT_TRIES) {
+            LOG.debug("Thread not finished, is kill now..");
+            thread.interrupt();
+            Thread.sleep(10);
+            count++;
+         }
+         if (count == INTERRUPT_TRIES) {
+            LOG.debug("Thread does not respond, so it is killed hard now.");
+            count = 0;
+            while (thread.isAlive() && count < 5) {
+               thread.stop();
+               Thread.sleep(10);
+               count++;
+            }
+         }
+      }
+   }
 
-	/**
-	 * Runs the whole testcase.
-	 * 
-	 * @param tr Where the results should be saved
-	 * @param warmupExecutions How many warmup executions should be done
-	 * @param executionTimes How many normal executions should be done
-	 * @param fullData Weather to log full data
-	 * @throws Throwable
-	 */
-	private void runTestCase(final TestResult tr, final int warmupExecutions, final int executionTimes, final boolean fullData)
-			throws Throwable {
+   /**
+    * Runs the whole testcase.
+    * 
+    * @param tr Where the results should be saved
+    * @param warmupExecutions How many warmup executions should be done
+    * @param executionTimes How many normal executions should be done
+    * @param fullData Weather to log full data
+    * @throws Throwable
+    */
+   private void runTestCase(final TestResult tr, final int warmupExecutions, final int executionTimes, final boolean fullData)
+         throws Throwable {
 
-		final String fullName = this.getClass().getName() + "." + getName();
-		try {
-			final TestResult bulkResult = new TestResult(tr.getTestcase(), executionTimes, getDataCollectors());
-			runMainExecution("warmup", fullName, bulkResult, warmupExecutions);
-			runMainExecution("main", fullName, tr, executionTimes);
-		} catch (final AssertionFailedError t) {
-			tr.finalizeCollection();
-			// PerformanceTestUtils.saveData(SaveableTestData.createAssertFailedTestData(getName(), getClass().getName(), tr, true));
-			throw t;
-		} catch (final Throwable t) {
-			tr.finalizeCollection();
-			// PerformanceTestUtils.saveData(SaveableTestData.createErrorTestData(getName(), getClass().getName(), tr, true));
-			throw t;
-		}
-	}
+      final String fullName = this.getClass().getName() + "." + getName();
+      try {
+         final TestResult bulkResult = new TestResult(tr.getTestcase(), executionTimes, getDataCollectors());
+         runMainExecution("warmup", fullName, bulkResult, warmupExecutions);
+         runMainExecution("main", fullName, tr, executionTimes);
+      } catch (final AssertionFailedError t) {
+         t.printStackTrace();
+         tr.finalizeCollection();
+         // PerformanceTestUtils.saveData(SaveableTestData.createAssertFailedTestData(getName(), getClass().getName(), tr, true));
+         throw t;
+      } catch (final Throwable t) {
+         t.printStackTrace();
+         tr.finalizeCollection();
+         // PerformanceTestUtils.saveData(SaveableTestData.createErrorTestData(getName(), getClass().getName(), tr, true));
+         throw t;
+      }
+   }
 
-	/**
-	 * Runs the main execution of the test, i.e.useKieker the execution where performance measures are counted.
-	 * 
-	 * @param testCase Runnable that should be run
-	 * @param name Name of the test
-	 * @param tr Where the results should be saved
-	 * @param executionTimes How often the test should be executed
-	 * @throws Throwable
-	 */
-	protected void runMainExecution(final String executionTypName, final String name, final TestResult tr, final int executionTimes) throws Throwable {
-		int executions;
-		final String firstPart = "--- Starting " + executionTypName + " execution " + name + " ";
-		final String firstPartStop = "--- Stopping " + executionTypName + " execution ";
-		final String endPart = "/" + executionTimes + " ---";
-		final int repetitions = getRepetitions();
-		for (executions = 1; executions <= executionTimes; executions++) {
-			LOG.debug(firstPart + executions + endPart);
-			tr.startCollection();
-			for (int repetion = 0; repetion < repetitions; repetion++) {
-				setUp();
-				KoPeMeTestcase.super.runTest();
-				tearDown();
-			}
+   /**
+    * Runs the main execution of the test, i.e.useKieker the execution where performance measures are counted.
+    * 
+    * @param testCase Runnable that should be run
+    * @param name Name of the test
+    * @param tr Where the results should be saved
+    * @param executionTimes How often the test should be executed
+    * @throws Throwable
+    */
+   protected void runMainExecution(final String executionTypName, final String name, final TestResult tr, final int executionTimes) throws Throwable {
+      int executions;
+      final String firstPart = "--- Starting " + executionTypName + " execution " + name + " ";
+      final String firstPartStop = "--- Stopping " + executionTypName + " execution ";
+      final String endPart = "/" + executionTimes + " ---";
+      final int repetitions = getRepetitions();
+      for (executions = 1; executions <= executionTimes; executions++) {
+         LOG.debug(firstPart + executions + endPart);
+         tr.startCollection();
+         for (int repetion = 0; repetion < repetitions; repetion++) {
+            setUp();
+            KoPeMeTestcase.super.runTest();
+            tearDown();
+         }
 
-			tr.stopCollection();
-			tr.getValue(TimeDataCollector.class.getName());
-			tr.setRealExecutions(executions);
-			LOG.debug(firstPartStop + executions + endPart);
-			if (Thread.interrupted()) {
-				return;
-			} else {
-				LOG.trace("Not interrupted");
-			}
-		}
-		LOG.debug("Executions: " + (executions - 1));
-		tr.setRealExecutions(executions - 1);
-	}
+         tr.stopCollection();
+         tr.getValue(TimeDataCollector.class.getName());
+         tr.setRealExecutions(executions);
+         LOG.debug(firstPartStop + executions + endPart);
+         if (Thread.interrupted()) {
+            return;
+         } else {
+            LOG.trace("Not interrupted");
+         }
+      }
+      LOG.debug("Executions: " + (executions - 1));
+      tr.setRealExecutions(executions - 1);
+   }
 }
