d9045e4c05b98221573dd13b24b376af61e31f99
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2019-03-05 17:28:12 +0100

    Enable KOPEME_CHUNKSTARTTIME

--- kopeme-core/src/main/java/de/dagere/kopeme/datastorage/XMLDataStorer.java
+++ kopeme-core/src/main/java/de/dagere/kopeme/datastorage/XMLDataStorer.java
@@ -18,6 +18,7 @@ import de.dagere.kopeme.generated.Result.Fulldata;
 import de.dagere.kopeme.generated.Result.Fulldata.Value;
 import de.dagere.kopeme.generated.TestcaseType;
 import de.dagere.kopeme.generated.TestcaseType.Datacollector;
+import de.dagere.kopeme.generated.TestcaseType.Datacollector.Chunk;
 
 /**
  * Manages the storing of resultdata of KoPeMe-tests in the KoPeMe-XML-format.
@@ -27,151 +28,160 @@ import de.dagere.kopeme.generated.TestcaseType.Datacollector;
  */
 public final class XMLDataStorer implements DataStorer {
 
-	private static final Logger LOG = LogManager.getLogger(XMLDataStorer.class);
-	private final File file;
-	private Kopemedata data;
+   private static final Logger LOG = LogManager.getLogger(XMLDataStorer.class);
+   private final File file;
+   private Kopemedata data;
 
-	/**
-	 * Initializes an XMLDataStorer.
-	 * 
-	 * @param foldername
-	 *            Folder where the result should be saved
-	 * @param classname
-	 *            Name of the test class which was executed
-	 * @param methodname
-	 *            Name of the method which was executed
-	 * @throws JAXBException
-	 *             Thrown if an XML Writing error occurs
-	 */
-	public XMLDataStorer(final File foldername, final String classname, final String methodname) throws JAXBException {
-		final String filename = methodname + ".xml";
-		file = new File(foldername, filename);
-		if (file.exists()) {
-			final XMLDataLoader loader = new XMLDataLoader(file);
-			data = loader.getFullData();
-		} else {
-			createXMLData(classname);
-		}
-	}
+   /**
+    * Initializes an XMLDataStorer.
+    * 
+    * @param foldername Folder where the result should be saved
+    * @param classname Name of the test class which was executed
+    * @param methodname Name of the method which was executed
+    * @throws JAXBException Thrown if an XML Writing error occurs
+    */
+   public XMLDataStorer(final File foldername, final String classname, final String methodname) throws JAXBException {
+      final String filename = methodname + ".xml";
+      file = new File(foldername, filename);
+      if (file.exists()) {
+         final XMLDataLoader loader = new XMLDataLoader(file);
+         data = loader.getFullData();
+      } else {
+         createXMLData(classname);
+      }
+   }
 
-	/**
-	 * Initializes XML-Data.
-	 * 
-	 * @param classname
-	 *            Name of the testclass
-	 */
-	public void createXMLData(final String classname) {
-		data = new Kopemedata();
-		data.setTestcases(new Testcases());
-		final Testcases tc = data.getTestcases();
-		tc.setClazz(classname);
-		storeData();
-	}
+   /**
+    * Initializes XML-Data.
+    * 
+    * @param classname Name of the testclass
+    */
+   public void createXMLData(final String classname) {
+      data = new Kopemedata();
+      data.setTestcases(new Testcases());
+      final Testcases tc = data.getTestcases();
+      tc.setClazz(classname);
+      storeData();
+   }
 
-	@Override
-	public void storeValue(final PerformanceDataMeasure performanceDataMeasure, final Map<Long, Long> values) {
-		if (data.getTestcases() == null) {
-			data.setTestcases(new Testcases());
-		}
-		final TestcaseType test = getOrCreateTestcase(performanceDataMeasure);
+   @Override
+   public void storeValue(final PerformanceDataMeasure performanceDataMeasure, final Map<Long, Long> values) {
+      if (data.getTestcases() == null) {
+         data.setTestcases(new Testcases());
+      }
+      final TestcaseType test = getOrCreateTestcase(performanceDataMeasure);
 
-		final Result r = new Result();
-		r.setDate(new Date().getTime());
-		r.setValue(performanceDataMeasure.value);
-		r.setDeviation(performanceDataMeasure.deviation);
-		r.setExecutionTimes(performanceDataMeasure.getExecutions());
-		r.setWarmupExecutions(performanceDataMeasure.getWarmup());
-		r.setRepetitions(performanceDataMeasure.getRepetitions());
-		r.setMax(performanceDataMeasure.max);
-		r.setMin(performanceDataMeasure.min);
-		r.setFirst10Percentile(performanceDataMeasure.first10percentile);
-		if (values != null) {
-			final Fulldata fd = new Fulldata();
-			for (final Map.Entry<Long, Long> valueEntry : values.entrySet()) {
-				final Value v = new Value();
-				v.setStart(valueEntry.getKey());
-				v.setValue("" + valueEntry.getValue());
-				fd.getValue().add(v);
-			}
-			r.setFulldata(fd);
-		}
+      final Result r = new Result();
+      r.setDate(new Date().getTime());
+      r.setValue(performanceDataMeasure.value);
+      r.setDeviation(performanceDataMeasure.deviation);
+      r.setExecutionTimes(performanceDataMeasure.getExecutions());
+      r.setWarmupExecutions(performanceDataMeasure.getWarmup());
+      r.setRepetitions(performanceDataMeasure.getRepetitions());
+      r.setMax(performanceDataMeasure.max);
+      r.setMin(performanceDataMeasure.min);
+      r.setFirst10Percentile(performanceDataMeasure.first10percentile);
+      if (values != null) {
+         final Fulldata fd = new Fulldata();
+         for (final Map.Entry<Long, Long> valueEntry : values.entrySet()) {
+            final Value v = new Value();
+            v.setStart(valueEntry.getKey());
+            v.setValue("" + valueEntry.getValue());
+            fd.getValue().add(v);
+         }
+         r.setFulldata(fd);
+      }
 
-		final Datacollector dc = getOrCreateDatacollector(performanceDataMeasure, test);
-		dc.getResult().add(r);
-	}
+      final Datacollector dc = getOrCreateDatacollector(performanceDataMeasure.getCollectorname(), test);
 
-	private Datacollector getOrCreateDatacollector(final PerformanceDataMeasure performanceDataMeasure, final TestcaseType test) {
-		Datacollector dc = null;
-		for (final Datacollector dc2 : test.getDatacollector()) {
-			LOG.trace("Name: {} Collectorname: {}", dc2.getName(), performanceDataMeasure.collectorname);
-			if (dc2.getName().equals(performanceDataMeasure.collectorname)) {
-				LOG.trace("Equals");
-				dc = dc2;
-			}
-		}
+      if (System.getenv("KOPEME_CHUNKSTARTTIME") != null) {
+         final long start = Long.parseLong(System.getenv("KOPEME_CHUNKSTARTTIME"));
+         Chunk current = null;
+         for (final Chunk chunk : dc.getChunk()) {
+            current = chunk;
+         }
+         if (current == null) {
+            current = new Chunk();
+            current.setChunkStartTime(start);
+            dc.getChunk().add(current);
+         }
+         current.getResult().add(r);
+      } else {
+         dc.getResult().add(r);
+      }
 
-		if (dc == null) {
-			LOG.trace("Erstelle neu");
-			dc = new Datacollector();
-			dc.setName(performanceDataMeasure.collectorname);
-			test.getDatacollector().add(dc);
-		}
-		return dc;
-	}
+   }
 
-	private TestcaseType getOrCreateTestcase(final PerformanceDataMeasure performanceDataMeasure) {
-		TestcaseType test = null;
-		for (final TestcaseType tc : data.getTestcases().getTestcase()) {
-			if (tc.getName().equals(performanceDataMeasure.testcase)) {
-				test = tc;
-			}
-		}
-		if (test == null) {
-			LOG.trace("Test == null, füge hinzu");
-			test = new TestcaseType();
-			test.setName(performanceDataMeasure.testcase);
-			data.getTestcases().getTestcase().add(test);
-		}
-		return test;
-	}
+   private Datacollector getOrCreateDatacollector(final String collector, final TestcaseType test) {
+      Datacollector dc = null;
+      for (final Datacollector dc2 : test.getDatacollector()) {
+         LOG.trace("Name: {} Collectorname: {}", dc2.getName(), collector);
+         if (dc2.getName().equals(collector)) {
+            LOG.trace("Equals");
+            dc = dc2;
+         }
+      }
 
-	@Override
-	public void storeData() {
-		JAXBContext jaxbContext;
-		try {
-			LOG.info("Storing data to: {}", file.getAbsoluteFile());
-			jaxbContext = JAXBContext.newInstance(Kopemedata.class);
-			final Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+      if (dc == null) {
+         LOG.trace("Erstelle neu");
+         dc = new Datacollector();
+         dc.setName(collector);
+         test.getDatacollector().add(dc);
+      }
+      return dc;
+   }
 
-			jaxbMarshaller.marshal(data, file);
-		} catch (final JAXBException e) {
-			e.printStackTrace();
-		}
-	}
+   private TestcaseType getOrCreateTestcase(final PerformanceDataMeasure performanceDataMeasure) {
+      TestcaseType test = null;
+      for (final TestcaseType tc : data.getTestcases().getTestcase()) {
+         if (tc.getName().equals(performanceDataMeasure.testcase)) {
+            test = tc;
+         }
+      }
+      if (test == null) {
+         LOG.trace("Test == null, füge hinzu");
+         test = new TestcaseType();
+         test.setName(performanceDataMeasure.testcase);
+         data.getTestcases().getTestcase().add(test);
+      }
+      return test;
+   }
 
-	/**
-	 * Stores the data in the given file.
-	 * 
-	 * @param file
-	 *            File for saving
-	 * @param currentdata
-	 *            Data to save
-	 */
-	public static void storeData(final File file, final Kopemedata currentdata) {
-		JAXBContext jaxbContext;
-		try {
-			LOG.info("Storing data to: {}", file.getAbsoluteFile());
-			jaxbContext = JAXBContext.newInstance(Kopemedata.class);
-			final Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-			jaxbMarshaller.setProperty("com.sun.xml.bind.indentString", " ");
-			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+   @Override
+   public void storeData() {
+      JAXBContext jaxbContext;
+      try {
+         LOG.info("Storing data to: {}", file.getAbsoluteFile());
+         jaxbContext = JAXBContext.newInstance(Kopemedata.class);
+         final Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+         jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 
-			jaxbMarshaller.marshal(currentdata, file);
-		} catch (final JAXBException e) {
-			e.printStackTrace();
-		}
-		LOG.debug("Storing finished.");
-	}
+         jaxbMarshaller.marshal(data, file);
+      } catch (final JAXBException e) {
+         e.printStackTrace();
+      }
+   }
+
+   /**
+    * Stores the data in the given file.
+    * 
+    * @param file File for saving
+    * @param currentdata Data to save
+    */
+   public static void storeData(final File file, final Kopemedata currentdata) {
+      JAXBContext jaxbContext;
+      try {
+         LOG.info("Storing data to: {}", file.getAbsoluteFile());
+         jaxbContext = JAXBContext.newInstance(Kopemedata.class);
+         final Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+         jaxbMarshaller.setProperty("com.sun.xml.bind.indentString", " ");
+         jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+         jaxbMarshaller.marshal(currentdata, file);
+      } catch (final JAXBException e) {
+         e.printStackTrace();
+      }
+      LOG.debug("Storing finished.");
+   }
 
 }
