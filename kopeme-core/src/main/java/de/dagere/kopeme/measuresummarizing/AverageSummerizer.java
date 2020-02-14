e16fb10457566981ab45cde88f687054e4ca7e9d
dgr <reichelt@informatik.uni-leipzig.de>
2020-02-14 17:43:33 +0100

    Cleanup

--- kopeme-core/src/main/java/de/dagere/kopeme/measuresummarizing/AverageSummerizer.java
+++ kopeme-core/src/main/java/de/dagere/kopeme/measuresummarizing/AverageSummerizer.java
@@ -1,9 +1,7 @@
 package de.dagere.kopeme.measuresummarizing;
 
-import java.util.Arrays;
 import java.util.List;
 
-import com.sun.xml.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 /**
  * A MeasureSummarizer for the average.
@@ -13,10 +11,6 @@ import com.sun.xml.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
  */
 public final class AverageSummerizer implements MeasureSummarizer {
 
-   public static void main(String[] args) {
-      System.out.println(new AverageSummerizer().getValue(Arrays.asList(5L, 6L)));
-   }
-   
 	@Override
 	public Number getValue(final List<Long> values) {
 		if (values != null && values.size() != 0) {
