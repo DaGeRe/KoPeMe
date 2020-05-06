0db553e893f4b8278a6c89b8967e825aba0d758a
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2020-05-06 23:55:31 +0200

    Cleanup CPU and RAM info

--- kopeme-core/src/main/java/de/dagere/kopeme/datastorage/EnvironmentUtil.java
+++ kopeme-core/src/main/java/de/dagere/kopeme/datastorage/EnvironmentUtil.java
@@ -10,7 +10,7 @@ public class EnvironmentUtil {
          try {
             Process process = new ProcessBuilder("/bin/sh", "-c", "cat /proc/cpuinfo | grep \"model name\" | uniq").start();
             try (Scanner scanner = new Scanner(process.getInputStream())) {
-               result = scanner.useDelimiter("\\A").next().replace("\t", " ");
+               result = scanner.useDelimiter("\\A").next().replace("\t", " ").replace("  ", "");
             }
          } catch (IOException e) {
             e.printStackTrace();
@@ -27,7 +27,7 @@ public class EnvironmentUtil {
          try {
             Process process = new ProcessBuilder("/bin/sh", "-c", "cat /proc/meminfo | grep \"MemTotal\"").start();
             try (Scanner scanner = new Scanner(process.getInputStream())) {
-               result = scanner.useDelimiter("\\A").next();
+               result = scanner.useDelimiter("\\A").next().replace("\t", " ").replace("  ", "");
             }
          } catch (IOException e) {
             e.printStackTrace();
