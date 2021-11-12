58cf5538586b674b36907f36b9010190490d7364
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2021-11-12 17:47:12 +0100

    Do not select alternative gradle files

--- kopeme-core/src/main/java/de/dagere/kopeme/parsing/GradleParseHelper.java
+++ kopeme-core/src/main/java/de/dagere/kopeme/parsing/GradleParseHelper.java
@@ -5,6 +5,8 @@ import java.io.FilenameFilter;
 
 public enum GradleParseHelper {
    ;
+   public static final String ALTERNATIVE_NAME = "alternative_build.gradle";
+   
    public static File findGradleFile(final File projectFolder) {
       File gradleFile = searchGradleFiles(projectFolder)[0];
       if (!gradleFile.exists()) {
@@ -16,13 +18,18 @@ public enum GradleParseHelper {
    public static File[] searchGradleFiles(final File projectFolder) {
       File gradleFile = new File(projectFolder, "build.gradle");
       if (!gradleFile.exists()) {
-         File[] gradleFiles = projectFolder.listFiles(new FilenameFilter() {
-            @Override
-            public boolean accept(final File dir, final String name) {
-               return name.endsWith(".gradle");
-            }
-         });
-         return gradleFiles;
+         File moduleNameCandidate = new File(projectFolder, projectFolder.getName() + ".gradle");
+         if (!moduleNameCandidate.exists()) {
+            File[] gradleFiles = projectFolder.listFiles(new FilenameFilter() {
+               @Override
+               public boolean accept(final File dir, final String name) {
+                  return name.endsWith(".gradle") && !name.equals(ALTERNATIVE_NAME);
+               }
+            });
+            return gradleFiles;
+         } else {
+            return new File[] { moduleNameCandidate };
+         }
       } else {
          return new File[] { gradleFile };
       }
