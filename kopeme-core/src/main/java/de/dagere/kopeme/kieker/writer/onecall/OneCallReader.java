5b37ac39eb10ba5a7cec37b8fac18142088af8c2
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-01-19 11:08:34 +0100

    Preserve method order

--- kopeme-core/src/main/java/de/dagere/kopeme/kieker/writer/onecall/OneCallReader.java
+++ kopeme-core/src/main/java/de/dagere/kopeme/kieker/writer/onecall/OneCallReader.java
@@ -4,13 +4,13 @@ import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
 public class OneCallReader {
    public static Set<String> getCalledMethods(final File folder) {
-      Set<String> methodNames = new HashSet<>();
+      Set<String> methodNames = new LinkedHashSet<>();
       for (File file : getFiles(folder)) {
          try {
             List<String> fileMethods = Files.readAllLines(file.toPath());
