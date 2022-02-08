0ef8cd82f7b51523e4901b9a1d5c0de9c3022838
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-02-08 21:22:08 +0100

    Add Mockito example test

---
+++ kopeme-junit/src/test/java/de/dagere/kopeme/junit5/TestJUnit5Mockito.java
@@ -0,0 +1,42 @@
+package de.dagere.kopeme.junit5;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.hamcrest.io.FileMatchers;
+import org.junit.jupiter.api.Test;
+
+import de.dagere.kopeme.datastorage.XMLDataLoader;
+import de.dagere.kopeme.generated.Kopemedata;
+import de.dagere.kopeme.junit5.exampletests.rules.ExampleExtension5MockitoTest;
+
+/**
+ * Tests just whether JUnit 5 execution works
+ * 
+ * @author reichelt
+ * 
+ */
+public class TestJUnit5Mockito {
+
+   public static Logger log = LogManager.getLogger(TestJUnit5Mockito.class);
+
+   @Test
+   public void testRegularExecution() throws JAXBException {
+      File file = JUnit5RunUtil.runJUnit5Test(ExampleExtension5MockitoTest.class);
+
+      MatcherAssert.assertThat("File " + file.getAbsolutePath() + " did not exist", file, FileMatchers.anExistingFile());
+
+      Kopemedata data = XMLDataLoader.loadData(file);
+      double averageDurationInMs = data.getTestcases().getTestcase().get(0).getDatacollector().get(0).getResult().get(0).getValue() / 1000000;
+      System.out.println(file.getAbsolutePath() + "=" + averageDurationInMs);
+
+      MatcherAssert.assertThat((int) averageDurationInMs, Matchers.greaterThan(20));
+   }
+
+   
+}
---
+++ kopeme-junit/src/test/java/de/dagere/kopeme/junit5/exampletests/rules/ExampleExtension5MockitoTest.java
@@ -0,0 +1,95 @@
+package de.dagere.kopeme.junit5.exampletests.rules;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+
+import de.dagere.kopeme.annotations.PerformanceTest;
+import de.dagere.kopeme.junit5.rule.KoPeMeExtension;
+
+class MyMockClazz{
+   @Mock
+   private Object value1;
+   
+   @Mock
+   private Object value2;
+   
+   public Object getValue1() {
+      return value1;
+   }
+   
+   public Object getValue2() {
+      return value2;
+   }
+}
+
+/**
+ * An example test for testing whether the KoPeMe-TestRule works correct
+ * 
+ * @author reichelt
+ *
+ */
+@ExtendWith(KoPeMeExtension.class)
+@ExtendWith(MockitoExtension.class)
+public class ExampleExtension5MockitoTest {
+
+   @Mock
+   private Runnable myMock;
+   
+   @Spy
+   private Runnable mySpy;
+   
+   @InjectMocks
+   private MyMockClazz mapper;
+   
+	@BeforeEach
+	public void setUp() {
+		System.out.println("Executing Setup");
+		try {
+			Thread.sleep(5);
+		} catch (final InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@AfterEach
+   public void tearDown() {
+      System.out.println("Executing Teardown");
+      try {
+         Thread.sleep(5);
+      } catch (final InterruptedException e) {
+         e.printStackTrace();
+      }
+   }
+
+	@Test
+	@PerformanceTest(warmup = 3, iterations = 3, repetitions = 1, timeout = 5000000, dataCollectors = "ONLYTIME", useKieker = false)
+	public void testNormal() {
+	   Mockito.doAnswer(new Answer<Void>() {
+
+         @Override
+         public Void answer(final InvocationOnMock invocation) throws Throwable {
+            System.out.println("Doing nothing");
+            return null;
+         }
+      }).when(myMock).run();
+	   
+	   System.out.println("Normal Execution");
+	   myMock.run();
+	   mySpy.run();
+	   System.out.println(mapper.getValue1() + " " + mapper.getValue2());
+	   try {
+         Thread.sleep(15);
+      } catch (InterruptedException e) {
+         e.printStackTrace();
+      }
+	}
+}
