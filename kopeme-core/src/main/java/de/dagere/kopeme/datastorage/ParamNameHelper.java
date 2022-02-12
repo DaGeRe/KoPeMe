a329e889a1ca618cdf4f7e25f17c6f94f73dbc96
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2022-02-12 22:50:04 +0100

    Extract constant name

--- kopeme-core/src/main/java/de/dagere/kopeme/datastorage/ParamNameHelper.java
+++ kopeme-core/src/main/java/de/dagere/kopeme/datastorage/ParamNameHelper.java
@@ -4,12 +4,15 @@ import de.dagere.kopeme.generated.Result.Params;
 import de.dagere.kopeme.generated.Result.Params.Param;
 
 public class ParamNameHelper {
+   
+   public static final String PARAM_VALUE_SEPARATOR = "-";
+   
    public static String paramsToString(final Params params) {
       String result;
       if (params != null) {
          result = "";
          for (Param param : params.getParam()) {
-            result += param.getKey() + "-" + param.getValue() + " ";
+            result += param.getKey() + PARAM_VALUE_SEPARATOR + param.getValue() + " ";
          }
          result = result.substring(0, result.length() - 1);
       } else {
