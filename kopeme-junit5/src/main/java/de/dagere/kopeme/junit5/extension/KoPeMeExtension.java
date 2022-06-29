4e5553435d6177472d25761b17ec59603688a07d
DaGeRe <davidgeorg_reichelt@dagere.de>
2022-06-29 17:23:41 +0200

    Add documentation for KoPeMeExtension

--- kopeme-junit5/src/main/java/de/dagere/kopeme/junit5/extension/KoPeMeExtension.java
+++ kopeme-junit5/src/main/java/de/dagere/kopeme/junit5/extension/KoPeMeExtension.java
@@ -4,6 +4,14 @@ import org.junit.jupiter.api.extension.ConditionEvaluationResult;
 import org.junit.jupiter.api.extension.ExecutionCondition;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
+/**
+ * This extension enables execution of JUnit 5 tests with KoPeMe. Since the execution scheme of JUnit 5 is strict, KoPeMe cannot directly decide when to execute
+ * BeforeAll, BeforeEach, Mockito initializations etc. Therefore, the KoPeMe extension is a ExecutionCondition and fully disabled the regular execution of a
+ * statement in JUnit 5, and starts the test execution by itself. If then the KoPeMe-started test execution reaches the ExecutionCondition-phase again, since we are
+ * inside KoPeMe, we need to set the test to enabled.
+ * @author reichelt
+ *
+ */
 public class KoPeMeExtension implements ExecutionCondition {
 
    private static boolean lastRunFailed = false;
