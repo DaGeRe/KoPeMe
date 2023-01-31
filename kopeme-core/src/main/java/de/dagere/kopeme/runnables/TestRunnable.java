2ba32d00cbd23642930af10c8138e10f2a72712a
DaGeRe <davidgeorg_reichelt@dagere.de>
2023-01-31 15:06:36 +0100

    Also see internal JUnit 5 exception as test error

--- kopeme-core/src/main/java/de/dagere/kopeme/runnables/TestRunnable.java
+++ kopeme-core/src/main/java/de/dagere/kopeme/runnables/TestRunnable.java
@@ -23,5 +23,11 @@ public interface TestRunnable {
     * @return After-Runnable
     */
    public KoPeMeThrowingRunnable getAfterRunnable();
+   
+   /**
+    * Returns an exception if one has been thrown (required for JUnit 5)
+    * 
+    */
+   public Throwable getThrowable();
 
 }
