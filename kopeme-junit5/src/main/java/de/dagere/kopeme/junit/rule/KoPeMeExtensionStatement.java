7bfd7045de5c08bd34f23c9f2ab49137614528e4
Matti Wilhelmi <maw@hbt.de>
2023-02-07 14:50:22 +0100

    Save testdata in KoPeMeExtensionStatement like in KoPeMeRuleStatement4 to createAssertFailedTestData also

--- kopeme-junit5/src/main/java/de/dagere/kopeme/junit/rule/KoPeMeExtensionStatement.java
+++ kopeme-junit5/src/main/java/de/dagere/kopeme/junit/rule/KoPeMeExtensionStatement.java
@@ -17,16 +17,15 @@ import de.dagere.kopeme.datastorage.ParamNameHelper;
 import de.dagere.kopeme.datastorage.RunConfiguration;
 import de.dagere.kopeme.datastorage.SaveableTestData;
 import de.dagere.kopeme.junit.rule.annotations.ParameterChecker;
-import de.dagere.kopeme.runnables.PreparableTestRunnables;
 import de.dagere.kopeme.runnables.TestRunnable;
+import junit.framework.AssertionFailedError;
 
 /**
  * Represents an execution of all runs of one test
- * 
+ *
  * TODO: Overthink weather directly configure test runs in KoPeMeRule would be more nice
- * 
+ *
  * @author dagere
- * 
  */
 public class KoPeMeExtensionStatement extends KoPeMeBasicStatement5 {
 
@@ -103,18 +102,18 @@ public class KoPeMeExtensionStatement extends KoPeMeBasicStatement5 {
          if (!isFinished) {
             runMainExecution(finalResult, "iteration ", annotation.iterations(), annotation.repetitions());
          }
+      } catch (final AssertionFailedError t) {
+         finalResult.finalizeCollection(t);
+         saveData(SaveableTestData.createAssertFailedTestData(finalResult.getMethodName(), clazzname, finalResult, configuration));
+         throw t;
       } catch (final Throwable t) {
          t.printStackTrace();
          finalResult.finalizeCollection(t);
          saveData(SaveableTestData.createErrorTestData(finalResult.getMethodName(), clazzname, finalResult, configuration));
          throw t;
       }
-      if (runnables.getThrowable() != null) {
-         saveData(SaveableTestData.createErrorTestData(finalResult.getMethodName(), clazzname, finalResult, configuration));
-      } else {
-         finalResult.finalizeCollection();
-         saveData(SaveableTestData.createFineTestData(finalResult.getMethodName(), clazzname, finalResult, configuration));
-      }
+      finalResult.finalizeCollection();
+      saveData(SaveableTestData.createFineTestData(finalResult.getMethodName(), clazzname, finalResult, configuration));
    }
 
    private void runWarmup() throws Throwable {
