6a0c5dba89b3e67760a768352583e57c2602d620
Matti Wilhelmi <maw@hbt.de>
2023-02-07 11:45:47 +0100

    Removed public modifiers and replaced Assert in TestJUnit5Throwing

--- kopeme-junit5/src/test/java/de/dagere/kopeme/junit5/TestJUnit5Throwing.java
+++ kopeme-junit5/src/test/java/de/dagere/kopeme/junit5/TestJUnit5Throwing.java
@@ -3,6 +3,7 @@ package de.dagere.kopeme.junit5;
 import java.io.File;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import de.dagere.kopeme.datastorage.JSONDataLoader;
@@ -11,25 +12,25 @@ import de.dagere.kopeme.junit5.exampletests.ExampleExtensionTestThrowing;
 import de.dagere.kopeme.junit5.extension.KoPeMeExtension;
 import de.dagere.kopeme.kopemedata.Kopemedata;
 
-public class TestJUnit5Throwing {
+class TestJUnit5Throwing {
    
    @Test
-   public void testCorrectThrowing() {
+   void testCorrectThrowing() {
       File file = JUnit5RunUtil.runJUnit5Test(ExampleExtensionTestThrowing.class);
       
-      Assert.assertTrue(KoPeMeExtension.isLastRunFailed());
+      Assertions.assertTrue(KoPeMeExtension.isLastRunFailed());
       
       Kopemedata data = JSONDataLoader.loadData(file);
-      Assert.assertTrue(data.getMethods().isEmpty());
+      Assertions.assertTrue(data.getMethods().isEmpty());
    }
    
    @Test
-   public void testCorrectFailure() {
+   void testCorrectFailure() {
       File file = JUnit5RunUtil.runJUnit5Test(ExampleExtensionTestFailure.class);
       
-      Assert.assertTrue(KoPeMeExtension.isLastRunFailed());
+      Assertions.assertTrue(KoPeMeExtension.isLastRunFailed());
       
       Kopemedata data = JSONDataLoader.loadData(file);
-      Assert.assertTrue(data.getMethods().isEmpty());
+      Assertions.assertTrue(data.getMethods().isEmpty());
    }
 }
