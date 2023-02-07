dadf9e222a98f320f5ff006bbbe1d664052a023d
Matti Wilhelmi <maw@hbt.de>
2023-02-07 11:46:58 +0100

    Reformat TestJUnit5Throwing

--- kopeme-junit5/src/test/java/de/dagere/kopeme/junit5/TestJUnit5Throwing.java
+++ kopeme-junit5/src/test/java/de/dagere/kopeme/junit5/TestJUnit5Throwing.java
@@ -2,7 +2,6 @@ package de.dagere.kopeme.junit5;
 
 import java.io.File;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -13,23 +12,23 @@ import de.dagere.kopeme.junit5.extension.KoPeMeExtension;
 import de.dagere.kopeme.kopemedata.Kopemedata;
 
 class TestJUnit5Throwing {
-   
+
    @Test
    void testCorrectThrowing() {
       File file = JUnit5RunUtil.runJUnit5Test(ExampleExtensionTestThrowing.class);
-      
+
       Assertions.assertTrue(KoPeMeExtension.isLastRunFailed());
-      
+
       Kopemedata data = JSONDataLoader.loadData(file);
       Assertions.assertTrue(data.getMethods().isEmpty());
    }
-   
+
    @Test
    void testCorrectFailure() {
       File file = JUnit5RunUtil.runJUnit5Test(ExampleExtensionTestFailure.class);
-      
+
       Assertions.assertTrue(KoPeMeExtension.isLastRunFailed());
-      
+
       Kopemedata data = JSONDataLoader.loadData(file);
       Assertions.assertTrue(data.getMethods().isEmpty());
    }
